26a5d209b8f0e2e72169185863239d1177a9596e
TOSHIBA <TOSHIBA@TOSHIBA>
2018-04-07 19:29:34 -0400

    update code

--- DataDrivenKeyWordDrivenAndHybridTesting/src/test/java/utility/DriverPath.java
+++ DataDrivenKeyWordDrivenAndHybridTesting/src/test/java/utility/DriverPath.java
@@ -5,5 +5,5 @@ public class DriverPath {
 	public static final String chromeDriver = currentDir + "\\src\\test\\java\\utility\\chromedriver.exe";
 	public static final String firefoxDriver = currentDir + "\\src\\test\\java\\utility\\geckodriver.exe";
 	public static final String excelPath = currentDir + "\\src\\test\\java\\testData\\TestCase.xlsx";
-	public static final  String objectProperties = currentDir + "\\src\\test\\java\\testData\\object.properties";
+	public static final String objectProperties = currentDir + "\\src\\test\\java\\testData\\object.properties";
 }
